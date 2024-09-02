0cdb2b52b27afb86355b88ec6e36f4acd85f3afe
mahmoudqtari <mahmoudqtari5@gmail.com>
2024-09-02 21:49:29 +0300

    create product database using first code

---
+++ Task1/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task1.Models;
+
+namespace Task1.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-2VPR9IB;Database=ASP9_EF;Trusted_Connection=true;TrustServerCertificate=True");
+        }
+        DbSet<Product> products {  get; set; }
+    }
+}
